e01fc07d3137a28e03e3026f4a33d1e44240faf8
wildeswt <mfsandoval.23@est.ucab.edu.ve>
2025-06-27 11:57:29 -0400

    feat: Sidebar funcional

--- frontend/myapp/react-src/src/App.jsx
+++ frontend/myapp/react-src/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Home from './pages/Home';
 import LoginRegister from './pages/LoginRegister';
+import Notifications from './pages/Notifications';
+import Security from './pages/Security';
+import Energy from './pages/Energy';
+import Settings from './pages/Settings';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
@@ -11,6 +15,10 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginRegister />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/security" element={<Security />} />
+          <Route path="/energy" element={<Energy />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </div>
     </Router>
